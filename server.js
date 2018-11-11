ed74798329ee64a9779db541bd2fb357567ed22c
Steve Jurcsek <MyNameTeve@users.noreply.github.com>
2018-11-10 23:04:26 -0500

    port config fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const DEV = process.argv.find((arg) => {return arg === '--dev';});
 const messages = DEV ? devConfig.messages : process.env; //is this chill?
 
 
-port = DEV ? devConfig.port : process.env.port;
+port = DEV ? devConfig.port : process.env.PORT;
 var server = http.createServer((request, response)=>{
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end(`${messages.helloWorld}`)
